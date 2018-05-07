0cf8d268a24548c181f3a3cf9c3f8999aefa2313
Joe Stafford <joestafford1986@gmail.com>
2018-05-07 13:29:14 +0100

    added edit form

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -8,3 +8,17 @@ get '/games' do
   @games = Game.all()
   erb ( :"games/index" )
 end
+
+#show singular game
+
+get '/games/:id' do
+  @game = Game.find(params[:id])
+  erb(:"games/show")
+end
+
+#go to edit form
+
+get '/games/:id/edit' do
+  @game = Game.find(params[:id])
+  erb(:"games/edit")
+end
