bf75ff7ebb7985c7cd9419f7b453c1f8a763ae89
Joe Stafford <joestafford1986@gmail.com>
2018-05-07 11:33:24 +0100

    added team edit

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require( 'sinatra/contrib/all' )
 require_relative('controllers/games_controller')
 require_relative('controllers/teams_controller')
 
+
+
 get '/' do
   erb( :index )
 end
--- controllers/teams_controller.rb
+++ controllers/teams_controller.rb
@@ -1,25 +1,34 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require( 'pry' )
 require_relative( '../models/team.rb' )
 
-#return all teams
+
+
+#show all teams
 get '/teams' do
   @teams = Team.all()
   erb ( :"teams/index" )
 end
+
+#show singular team
 
 get '/teams/:id' do
   @team = Team.find(params[:id])
   erb(:"teams/show")
 end
 
+#go to edit form
+
 get '/teams/:id/edit' do
   @team = Team.find(params[:id])
   erb(:"teams/edit")
 end
 
+#run update on edited team
+
 post '/teams/:id/edit' do
-  @team = Team.new(params)
-  @team.update()
+  team = Team.new(params)
+  team.update()
   redirect '/'
 end
