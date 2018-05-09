bb50f719f4f3902ec43f95d8449f354acf0ac9a0
Joe Stafford <joestafford1986@gmail.com>
2018-05-09 17:11:51 +0100

    removed data_manager controller

--- app.rb
+++ app.rb
@@ -2,8 +2,10 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/games_controller')
 require_relative('controllers/teams_controller')
-require_relative('controllers/data_managers_controller')
+require_relative('models/data_manager')
 
 get '/' do
+  sorted_teams = DataManager.sort_by_wins()
+  @sorted_teams_wins_then_goals = DataManager.sort_teams()
   erb( :index )
 end
--- models/data_manager.rb
+++ models/data_manager.rb
@@ -12,6 +12,7 @@ class DataManager
   def self.sort_teams()
     teams = Team.all()
     sorted = teams.sort_by{|team| [team.get_wins_count(), team.get_goal_difference()]}
+    return sorted.reverse()
   end
 
 end
