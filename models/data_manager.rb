96e93888af097863db61cbdd24f2982ad47c5af5
Joe Stafford <joestafford1986@gmail.com>
2018-05-09 23:25:17 +0100

    moved elements / removed redundant methods

--- models/data_manager.rb
+++ models/data_manager.rb
@@ -3,12 +3,6 @@ require_relative('game')
 
 class DataManager
 
-  def self.sort_by_wins()
-    teams = Team.all()
-    sorted_teams = teams.sort {|team1, team2| team2.get_wins_count() <=> team1.get_wins_count()}
-    return sorted_teams
-  end
-
   def self.sort_teams()
     teams = Team.all()
     sorted = teams.sort_by{|team| [team.get_wins_count(), team.get_goal_difference()]}
--- models/team.rb
+++ models/team.rb
@@ -210,21 +210,6 @@ class Team
     return games
   end
 
-  # def self.sort_by_wins()
-  #   teams = Team.all()
-  #   sorted_teams = teams.sort {|team1, team2| team2.get_wins_count() <=> team1.get_wins_count()}
-  #   return sorted_teams
-  # end
-  #
-  # def self.sort_by_goal_difference(teams)
-  #   sorted_teams_by_goals = teams.sort do |team1, team2|
-  #     if team2.get_wins_count() == team1.get_wins_count()
-  #       team2.get_goal_difference() <=> team1.get_goal_difference()
-  #     end
-  #   end
-  #   return sorted_teams_by_goals
-  # end
-
   def self.all()
     sql = "SELECT * FROM teams"
     results = SqlRunner.run( sql )
